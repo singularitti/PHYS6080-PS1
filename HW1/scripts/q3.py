02fc1434ab9496b9b4f5dbba05d50eefaa2d0f78
singularitti <singularitti@outlook.com>
2022-09-16 20:45:01 -0400

    Add q3.py

---
+++ HW1/scripts/q3.py
@@ -0,0 +1,65 @@
+# Back recursion for modified Bessel fucntions
+from decimal import Decimal, getcontext
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import special
+
+getcontext().prec = 50
+
+max_order = 30
+orders = range(1, max_order + 1)
+x = 5
+
+
+def coeff(i):
+    if i == 0:
+        return 1
+    elif i % 4 == 0:
+        return 2
+    elif i % 2 == 0:
+        return -2
+    else:
+        return 0
+
+
+def normalize(I):
+    coeffs = np.vectorize(coeff)(range(len(I)))
+    scaling_factor = np.dot(coeffs, I)
+    return I / scaling_factor
+
+
+def back_recursion(x, max_order, init=(1, 0)):
+    I = np.empty(max_order)
+    I[-2:] = init
+    for v in np.flip(orders)[2:]:  # Orders 28-1
+        I[v - 1] = I[v + 1] + 2 * v / x * I[v]
+    return normalize(I)
+
+
+def back_recursion_precise(x, max_order, init=(Decimal(1.0), Decimal(0.0))):
+    orders = range(1, max_order + 1)
+    I = np.array(list(map(Decimal, orders)))
+    I[-2:] = map(Decimal, init)
+    for v in np.flip(orders)[2:]:  # Orders 28-1
+        I[v - 1] = I[v + 1] + 2 * v / Decimal(x) * I[v]
+    return normalize(I)
+
+
+I = back_recursion(x, max_order, (1, 0))
+I_dec = back_recursion(x, max_order, (Decimal(1.0), Decimal(0.0)))
+I_exact = np.array([special.iv(order - 1, x) for order in orders])
+
+plt.plot(orders, I, label="My rough algorithm")
+plt.plot(orders, I_dec, label="My rough algorithm with higher precision")
+plt.plot(orders, I_exact, label="Exact values")
+plt.xlim((1, max_order))
+plt.legend(loc="best")
+
+plt.figure()
+plt.plot(orders, np.divide(I, I_exact), label="My rough algorithm")
+plt.plot(orders, np.divide(I_dec, I_exact),
+         label="My rough algorithm with higher precision")
+plt.xlim((1, max_order))
+plt.legend(loc="best")
+# plt.show()
