933f8f8dfa67782df8c47874b5e8ba91e393c4c9
singularitti <singularitti@outlook.com>
2022-09-17 08:50:22 -0400

    Update HW1/scripts/q3.py

--- HW1/scripts/q3.py
+++ HW1/scripts/q3.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import special
 
-from src.bessel_functions import back_recursion
+from src.bessel_functions import back_recursion, errors, max_errors
 from src.plotting import figpath
 
 
 def prepare_data(x, max_order):
     orders = range(1, max_order + 1)
-    my = back_recursion(x, max_order, (1, 0))
+    my = back_recursion(x, max_order)
     exact = np.array([special.iv(order - 1, x) for order in orders])
     return my, exact
 
@@ -17,30 +17,46 @@ def prepare_data(x, max_order):
 def plot_raw(x, max_order):
     orders = range(1, max_order + 1)
     my, exact = prepare_data(x, max_order)
-    plt.figure()
-    plt.scatter(orders, my, label="A naïve algorithm")
-    plt.scatter(orders, exact, label="Exact values by SciPy")
-    plt.xlim((1, max_order))
-    plt.xlabel(r"back recursion steps ($n$)")
-    plt.ylabel(r"$I_{n}(x=5)$")
-    plt.legend(loc="best")
-    plt.savefig(figpath("q3_1.pdf"))
+    fig, ax = plt.subplots()
+    ax.scatter(orders, my, label="A naïve algorithm")
+    ax.scatter(orders, exact, label="Exact values by SciPy")
+    ax.set_xlim((1, max_order))
+    ax.set_xlabel(r"back recursion steps ($n$)")
+    ax.set_ylabel(r"$I_{n}(x=5)$")
+    ax.legend(loc="best")
+    fig.savefig(figpath("q3_1.pdf"))
+    return fig, ax
 
 
-def plot_diff(x, max_order):
+def plot_errors(x, max_order):
     orders = range(1, max_order + 1)
-    my, exact = prepare_data(x, max_order)
-    plt.figure()
-    plt.scatter(orders, my - exact)
-    plt.xlim((1, max_order))
-    plt.xlabel(r"back recursion steps ($n$)")
-    plt.ylabel(r"$\Delta = I_{n}(x=5) - I_{n,\textnormal{exact}}(x=5)$")
-    plt.savefig(figpath("q3_2.pdf"))
+    fig, ax = plt.subplots()
+    ax.scatter(orders, errors(x, max_order, back_recursion))
+    ax.set_xlim((1, max_order))
+    ax.set_xlabel(r"back recursion steps ($n$)")
+    ax.set_ylabel(r"$\Delta = I_{n}(x=5) - I_{n,\textnormal{exact}}(x=5)$")
+    fig.savefig(figpath("q3_2.pdf"))
+    return fig, ax
+
+
+def plot_mat_errors(method):
+    xmin, xmax, ymin, ymax = 1, 11, 3, 20
+    xrange, yrange = list(range(xmin, xmax)), list(range(ymin, ymax))
+    mat_errors = max_errors(xrange, yrange, method)
+    mat_errors[mat_errors > 1] = 0
+    fig, ax = plt.subplots()
+    ax.imshow(mat_errors, aspect="equal", origin="lower", extent=[xmin, xmax, ymin, ymax])
+    ax.set_xticks(xrange)
+    ax.set_yticks(yrange)
+    plt.colorbar()
+    plt.savefig(figpath("q3_3.pdf"))
+    return fig, ax
 
 
 def main(x, max_order):
     plot_raw(x, max_order)
-    plot_diff(x, max_order)
+    plot_errors(x, max_order)
+    plot_mat_errors(back_recursion)
 
 
 if __name__ == "__main__":
