dc9369d08a53119e2445f883887c764c02a066af
singularitti <singularitti@outlook.com>
2022-09-17 08:49:06 -0400

    Deprecate `init` args in `back_recursion` & `back_recursion_precise`

--- HW1/src/bessel_functions.py
+++ HW1/src/bessel_functions.py
@@ -27,17 +27,17 @@ def orders(max_order):
     return range(1, max_order + 1)
 
 
-def back_recursion(x, max_order, init=(1, 0)):
+def back_recursion(x, max_order):
     I = np.empty(max_order)  # Initialize a vector of size `max_order`
-    I[-2:] = init  # Set the last two values to `init`
+    I[-2:] = 1, 0  # Set the last two values to `init`
     for n in np.flip(orders(max_order))[2:]:  # Orders from `max_order-2` to 1
         I[n - 1] = I[n + 1] + 2 * n / x * I[n]
     return normalize(I)
 
 
-def back_recursion_precise(x, max_order, init=(Decimal(1.0), Decimal(0.0))):
+def back_recursion_precise(x, max_order):
     I = np.array(list(map(Decimal, orders(max_order))))
-    I[-2:] = map(Decimal, init)
-    for n in np.flip(orders(max_order))[2:]:  # Orders 28-1
+    I[-2:] = Decimal(1.0), Decimal(0.0)
+    for n in np.flip(orders(max_order))[2:]:  # Orders from `max_order-2` to 1
         I[n - 1] = I[n + 1] + 2 * n / Decimal(x) * I[n]
     return normalize(I)
