b33984163dbfdc1657656a7c6c61aa7739653cfb
singularitti <singularitti@outlook.com>
2022-09-18 08:22:55 -0400

    Deprecate `orders`, fix `back_recursion` & `back_recursion_precise` recurrence rules

--- code/ps1/bessel_functions.py
+++ code/ps1/bessel_functions.py
@@ -26,34 +26,32 @@ def normalize(bessel_functions):
     return bessel_functions / scaling_factor  # Divide all values in $\bm{I}$ by $s$
 
 
-def orders(max_order):
-    return range(1, max_order + 1)
-
-
-def back_recursion(x, max_order):
-    I = np.empty(max_order)  # Initialize a vector of size `max_order`
+def back_recursion(x, last_order):
+    orders = range(last_order + 1)
+    I = np.empty(last_order + 1)  # Orders from 0 to `last_order`
     I[-2:] = 1, 0  # Set the last two values
-    for n in np.flip(orders(max_order))[2:]:  # Orders from `max_order-2` to 1
-        I[n - 1] = I[n + 1] + 2 * n / x * I[n]
+    for n in np.flip(orders)[2:]:  # Orders from `last_order-2` to 0
+        I[n] = I[n + 2] + 2 * (n + 1) / x * I[n + 1]
     return normalize(I)
 
 
-def back_recursion_precise(x, max_order):
-    I = np.array(list(map(Decimal, orders(max_order))))
-    I[-2:] = Decimal(1.0), Decimal(0.0)
-    for n in np.flip(orders(max_order))[2:]:  # Orders from `max_order-2` to 1
-        I[n - 1] = I[n + 1] + 2 * n / Decimal(x) * I[n]
+def back_recursion_precise(x, last_order):
+    orders = range(last_order + 1)
+    I = np.fromiter(map(Decimal, orders), Decimal)
+    I[-2:] = Decimal(1), Decimal(0)
+    for n in np.flip(orders)[2:]:  # Orders from `last_order-2` to 0
+        I[n] = I[n + 2] + 2 * (n + 1) / Decimal(x) * I[n + 1]
     return normalize(I)
 
 
-def errors(x, max_order, method=back_recursion):
-    I = method(x, max_order)
-    I_exact = np.array([special.iv(order - 1, x) for order in orders(max_order)])
+def errors(x, last_order, method=back_recursion):
+    I = method(x, last_order)
+    I_exact = np.array([special.iv(order, x) for order in range(last_order + 1)])
     return abs(I - I_exact)
 
 
 def max_errors(xrange, order_range, method):
-    return np.array([[errors(x, max_order, method).max() for x in xrange] for max_order in order_range])
+    return np.array([[errors(x, last_order, method).max() for x in xrange] for last_order in order_range])
 
 
 def minimum_steps(xrange, order_range, method):
