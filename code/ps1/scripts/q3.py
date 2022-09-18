7557b5b14d1db8c35f10ec5da1a07b25b9c76727
singularitti <singularitti@outlook.com>
2022-09-18 06:13:31 -0400

    Reorder code structures

--- code/ps1/scripts/q3.py
+++ code/ps1/scripts/q3.py
@@ -1,6 +1,6 @@
 # Back recursion for modified Bessel functions
-from src.bessel_functions import back_recursion
-from src.plotting import plot_exact, plot_errors, plot_mat_errors, plot_raw
+from ps1.bessel_functions import back_recursion
+from ps1.plotting import plot_exact, plot_errors, plot_mat_errors, plot_raw
 
 
 def main(x, max_order):
