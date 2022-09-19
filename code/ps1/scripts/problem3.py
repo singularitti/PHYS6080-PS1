ba0093884597152671967586978b78f6ef606d87
singularitti <singularitti@outlook.com>
2022-09-19 19:28:30 -0400

    Rename q3.py to code/ps1/scripts/problem3.py

--- code/ps1/scripts/problem3.py
+++ code/ps1/scripts/problem3.py
@@ -1,6 +1,6 @@
 # Back recursion for modified Bessel functions
 from ps1.bessel_functions import back_recursion
-from ps1.plotting import plot_exact, plot_errors, plot_mat_errors, plot_raw
+from ps1.plotting import plot_exact, plot_errors, save_plots_gif, plot_mat_errors, plot_raw
 
 
 def main(x, max_order):
@@ -10,5 +10,6 @@ def main(x, max_order):
 
 
 if __name__ == "__main__":
-    for y in range(10, 40):
-        main(10, y)
+    pass
+    # for y in range(10, 40):
+    #     main(10, y)
