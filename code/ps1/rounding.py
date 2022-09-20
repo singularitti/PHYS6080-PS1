c38bf19055cff6107a90869d4a056ffe104fe918
singularitti <singularitti@outlook.com>
2022-09-20 02:56:17 -0400

    Fix `rounded_mul` & `truncated_multiply`

--- code/ps1/rounding.py
+++ code/ps1/rounding.py
@@ -5,7 +5,7 @@
 import math
 
 
-__all__ = ['myround', 'truncate', 'rounded_multiply', 'truncated_multiply']
+__all__ = ['myround', 'truncate', 'rounded_mul', 'truncated_mul']
 
 
 def myround(x, ndigits):
@@ -17,12 +17,10 @@ def truncate(x, ndigits):
     return int(x * 10**ndigits) * 10**(-ndigits)
 
 
-def rounded_multiply(x, y, ndigits):
+def rounded_mul(x, y, ndigits):
     x, y = myround(x, ndigits), myround(y, ndigits)
-    z = x * y
-    return myround(z, ndigits)
-
-
-def truncated_multiply(x, y, ndigits):
-    x, y = truncate(x, ndigits), truncate(y, ndigits)
     return x * y
+
+
+def truncated_mul(x, y, ndigits):
+    return x * truncate(y, ndigits)
