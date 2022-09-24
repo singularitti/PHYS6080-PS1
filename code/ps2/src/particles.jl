f3ee445d28e901ad975a035df6abda8fb594592b
Reno <singularitti@outlook.com>
2022-09-24 18:36:40 -0400

    Add 2 methods to `force`

--- code/ps2/src/particles.jl
+++ code/ps2/src/particles.jl
@@ -23,3 +23,11 @@ function force(p1::Particle, p2::Particle)
     η = 1 / distance(p1, p2)
     return (p1.r - p2.r) * (η^14 - η^8)
 end
+function force(particles, i)
+    @assert length(particles) > 1 "you must have more than 1 particle to calculate the force!"
+    particleᵢ = particles[i]
+    return sum(enumerate(particles)) do (j, particleⱼ)
+        j != i ? force(particleᵢ, particleⱼ) : zeros(MVector{3,Float64})
+    end
+end
+force(particles) = map(Base.Fix1(force, particles), eachindex(particles))
