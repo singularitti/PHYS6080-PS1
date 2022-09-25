f0511f729992c9f36f81e1b78759a5f1de891a81
singularitti <singularitti@outlook.com>
2022-09-25 02:36:37 -0400

    Update `temperature`

--- code/ps2/src/thermal.jl
+++ code/ps2/src/thermal.jl
@@ -1,7 +1,11 @@
-function temperature(particles)
-    N = length(particles)
-    velocities = map(Base.Fix2(getfield, :v), particles)
-    return 16 / N * sum(abs2, velocities)
+export temperature
+
+function temperature(particles::Particles)
+    velocities = map(particles) do particle
+        particle.v
+    end
+    n = length(particles)
+    return 16 / n * sum(abs2, velocities)
 end
 
 volume(particles) = length(particles) / ρ
